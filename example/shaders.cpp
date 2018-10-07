f6fbabba29e6c0d2aaeaaf5637d1b1baffedf45c
Chris Glover <c.d.glover@gmail.com>
2018-10-07 14:59:05 -0400

    First attempt at imguizmo

--- example/shaders.cpp
+++ example/shaders.cpp
@@ -15,6 +15,7 @@
 #include "swgl/shaders/gouraud.hpp"
 #include "swgl/shaders/phong.hpp"
 
+#include "ImGuizmo/ImGuizmo.h"
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl2.h"
@@ -225,6 +226,8 @@ class application {
     ImGui_ImplOpenGL2_NewFrame();
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
+    ImGuizmo::BeginFrame();
+    ImGuizmo::SetOrthographic(false);
 
     // Options window
     {
@@ -241,7 +244,8 @@ class application {
 
       ImGui::Separator();
       ImGui::Text("Light Direction");
-      ImGui::SliderFloat2("LThetaPhi", dir_light_theta_phi_.raw.data(), -1.f, 1.f);
+      ImGui::SliderFloat2(
+          "LThetaPhi", dir_light_theta_phi_.raw.data(), -1.f, 1.f);
 
       ImGui::ColorEdit3(
           "Clear Colour",
@@ -267,9 +271,100 @@ class application {
       ImGui::End();
     }
 
+    // Draw Manipulators
+    {
+      auto view = get_view(get_eye());
+      view.transpose();
+      auto proj = get_projection();
+      proj.transpose();
+      static auto o1 = swgl::matrix4f::identity();
+      //o1.transpose();
+      EditTransform(view.raw.data(), proj.raw.data(), o1.raw.data());
+      ImGuizmo::DrawCube(view.raw.data(), proj.raw.data(), o1.raw.data());
+      ImGuizmo::DrawGrid(
+          view.raw.data(), proj.raw.data(),
+          swgl::matrix4f::identity().raw.data(), 10.f);
+      //o1.transpose();
+    }
+
     //    ImGui::ShowDemoWindow();
 
     ImGui::Render();
+  }
+
+  void EditTransform(
+      const float* cameraView, float* cameraProjection, float* matrix) {
+    static ImGuizmo::OPERATION mCurrentGizmoOperation(ImGuizmo::TRANSLATE);
+    static ImGuizmo::MODE mCurrentGizmoMode(ImGuizmo::LOCAL);
+    static bool useSnap         = false;
+    static float snap[3]        = {1.f, 1.f, 1.f};
+    static float bounds[]       = {-0.5f, -0.5f, -0.5f, 0.5f, 0.5f, 0.5f};
+    static float boundsSnap[]   = {0.1f, 0.1f, 0.1f};
+    static bool boundSizing     = false;
+    static bool boundSizingSnap = false;
+
+    if(ImGui::IsKeyPressed(90))
+      mCurrentGizmoOperation = ImGuizmo::TRANSLATE;
+    if(ImGui::IsKeyPressed(69))
+      mCurrentGizmoOperation = ImGuizmo::ROTATE;
+    if(ImGui::IsKeyPressed(82)) // r Key
+      mCurrentGizmoOperation = ImGuizmo::SCALE;
+    if(ImGui::RadioButton(
+           "Translate", mCurrentGizmoOperation == ImGuizmo::TRANSLATE))
+      mCurrentGizmoOperation = ImGuizmo::TRANSLATE;
+    ImGui::SameLine();
+    if(ImGui::RadioButton("Rotate", mCurrentGizmoOperation == ImGuizmo::ROTATE))
+      mCurrentGizmoOperation = ImGuizmo::ROTATE;
+    ImGui::SameLine();
+    if(ImGui::RadioButton("Scale", mCurrentGizmoOperation == ImGuizmo::SCALE))
+      mCurrentGizmoOperation = ImGuizmo::SCALE;
+    float matrixTranslation[3], matrixRotation[3], matrixScale[3];
+    ImGuizmo::DecomposeMatrixToComponents(
+        matrix, matrixTranslation, matrixRotation, matrixScale);
+    ImGui::InputFloat3("Tr", matrixTranslation, 3);
+    ImGui::InputFloat3("Rt", matrixRotation, 3);
+    ImGui::InputFloat3("Sc", matrixScale, 3);
+    ImGuizmo::RecomposeMatrixFromComponents(
+        matrixTranslation, matrixRotation, matrixScale, matrix);
+
+    if(mCurrentGizmoOperation != ImGuizmo::SCALE) {
+      if(ImGui::RadioButton("Local", mCurrentGizmoMode == ImGuizmo::LOCAL))
+        mCurrentGizmoMode = ImGuizmo::LOCAL;
+      ImGui::SameLine();
+      if(ImGui::RadioButton("World", mCurrentGizmoMode == ImGuizmo::WORLD))
+        mCurrentGizmoMode = ImGuizmo::WORLD;
+    }
+    if(ImGui::IsKeyPressed(83))
+      useSnap = !useSnap;
+    ImGui::Checkbox("", &useSnap);
+    ImGui::SameLine();
+
+    switch(mCurrentGizmoOperation) {
+    case ImGuizmo::TRANSLATE:
+      ImGui::InputFloat3("Snap", &snap[0]);
+      break;
+    case ImGuizmo::ROTATE:
+      ImGui::InputFloat("Angle Snap", &snap[0]);
+      break;
+    case ImGuizmo::SCALE:
+      ImGui::InputFloat("Scale Snap", &snap[0]);
+      break;
+    }
+    ImGui::Checkbox("Bound Sizing", &boundSizing);
+    if(boundSizing) {
+      ImGui::PushID(3);
+      ImGui::Checkbox("", &boundSizingSnap);
+      ImGui::SameLine();
+      ImGui::InputFloat3("Snap", boundsSnap);
+      ImGui::PopID();
+    }
+
+    ImGuiIO& io = ImGui::GetIO();
+    ImGuizmo::SetRect(0, 0, io.DisplaySize.x, io.DisplaySize.y);
+    ImGuizmo::Manipulate(
+        cameraView, cameraProjection, mCurrentGizmoOperation, mCurrentGizmoMode,
+        matrix, NULL, useSnap ? &snap[0] : NULL, boundSizing ? bounds : NULL,
+        boundSizingSnap ? boundsSnap : NULL);
   }
 
   void present() const {
